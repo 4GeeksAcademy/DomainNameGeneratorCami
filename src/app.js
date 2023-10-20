b1e8070d3995b74ac7c37fb7f6307b6e818f9a20
0keeffee <137979040+0keeffee@users.noreply.github.com>
2023-10-20 21:47:20 +0000

    random domain names

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const pronouns = ["the", "your"];
+const adjectives = ["awesome", "tiny"];
+const nouns = ["dinosaur", "buble"];
+const extensions = [".ch", ".co", ".media", ".io"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const domainNamesContainer = document.querySelector(".domain-names");
+
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let domainName =
+            pronouns[i] + adjectives[j] + nouns[k] + extensions[l];
+
+          let domainNameElement = document.createElement("p");
+          domainNameElement.textContent = domainName;
+
+          domainNamesContainer.appendChild(domainNameElement);
+
+          let domainHack =
+            pronouns[i] + adjectives[j] + "." + nouns[k] + extensions[l];
+
+          let domainHackElement = document.createElement("p");
+          domainHackElement.textContent = domainHack;
+
+          domainNamesContainer.appendChild(domainHackElement);
+        }
+      }
+    }
+  }
 };
